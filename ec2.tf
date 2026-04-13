14aaefca3af168c4a0dc6de43515358ba0603a10
Nito Martinez <353827+nitomartinez@users.noreply.github.com>
2026-04-13 11:18:32 +0000

    Remove ec2 resource

--- ec2.tf
+++ ec2.tf
@@ -2,7 +2,8 @@ provider "aws" {
     region = "eu-south-2"
 }
 
-resource "aws_instance" "myec2" {
-    ami = "ami-0700e097bd263d73e"
-    instance_type = "t3.micro"
-}
+# Remove resource
+# resource "aws_instance" "myec2" {
+#     ami = "ami-0700e097bd263d73e"
+#     instance_type = "t3.micro"
+# }
