9e5e4b241db0fb7dc49ea1f46228b2cc2279451b
Nito Martinez <353827+nitomartinez@users.noreply.github.com>
2026-04-20 05:24:08 +0000

    removal

--- ec2.tf
+++ ec2.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 # Remove resource
-resource "aws_instance" "myec2" {
-    ami = "ami-0700e097bd263d73e"
-    instance_type = "t3.micro"
-}
+# resource "aws_instance" "myec2" {
+#     ami = "ami-0700e097bd263d73e"
+#     instance_type = "t3.micro"
+# }
